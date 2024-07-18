cb39b3f52fb56f60205ced5a3ae3f086f2b3dc72
sliightDriizzle <capybara0711@gmail.com>
2024-07-17 20:39:09 -0700

    fixing link

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -5,12 +5,14 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/Link';
+import Link from 'next/link';
+// import { chakra, LinkProps } from '@chakra-ui/react';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
+
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
